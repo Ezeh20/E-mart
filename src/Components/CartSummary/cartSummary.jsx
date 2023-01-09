1f92bd97281965b64383120e0e61a0fa713408c8
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-01-09 16:51:49 +0100

    Refactor: Changing up the checkout UI

--- src/Components/CartSummary/cartSummary.jsx
+++ src/Components/CartSummary/cartSummary.jsx
@@ -1,5 +1,5 @@
 
-
+import Button from '../Button-component/button.component'
 import './cartSummary.scss'
 
 
@@ -8,10 +8,20 @@ const CartSummary = () => {
         <div className="sub_total_car">
             <div className="car_cart">
                 <p className="cart_sum">CART SUMMARY</p>
+                <div className="line_checkout">
+                    <div className="line_line"></div>
+                </div>
             </div>
             <div className="sub_total">
-
+                <div className="sub_sub">
+                    <p className="sub_tot">Subtotal</p>
+                    <p className="total_amt">#5000</p>
+                </div>
+                <div className="line_checkout">
+                    <div className="line_line"></div>
+                </div>
             </div>
+            <Button buttonType={`primary`}>Checkout ($9000)</Button>
         </div>
     )
 }
